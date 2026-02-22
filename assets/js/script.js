8c1613f7a62fdc4039127395dbdfda17f051e0f1
jjortiz-bi <60684514+jjortiz-bi@users.noreply.github.com>
2026-02-22 12:34:10 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,12 @@ const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("navMenu");
 const header = document.getElementById("mainHeader");
 
+// Toggle menu
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 });
 
+// Shrink header on scroll
 window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
         header.classList.add("shrink");
